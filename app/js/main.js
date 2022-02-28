59a19d5785f1066b6c958d9b9801377f3fd23f42
Alex Kuvaldin <kuvaldin@5th.ru>
2022-02-28 23:15:36 +0700

    adaptive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
-function name(params) {
-    
+// burger
+
+function toggleBurger() {
+  const burger = document.querySelector('.header__burger');
+  const navMobile = document.querySelector('.header__nav-wrapper');
+  if (!burger) {
+    return;
+  }
+
+  burger.addEventListener('click', e => {
+    e.preventDefault();
+    burger.classList.toggle('header__burger--active');
+    navMobile.classList.toggle('header__nav-wrapper--active');
+  });
+
 }
+
+
+toggleBurger();
